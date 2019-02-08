275efb198e591d47246e3d1da612465058df550a
rummolprod999 <rummolprod999@gmail.com>
2019-02-08 11:08:11 +0300

    first commit

---
+++ Program.fs
@@ -0,0 +1,130 @@
+﻿namespace ParserFsharp
+open System
+
+type ListBuilder() =
+    member this.Bind(m, f) =
+        printfn "Bind fun execute %A" m
+        m |> List.collect f
+
+    member this.Zero() = 
+        printfn "Zero"
+        []
+
+    member this.Return(x) = 
+        printfn "Return an unwrapped %A as a list" x
+        [x]
+
+    member this.Yield(x) = 
+        printfn "Yield an unwrapped %A as a list" x
+        [x]
+        
+    member this.For(m,f) =
+        printfn "For %A" m
+        this.Bind(m, f)
+ 
+ type TraceBuilderWhile() =
+    member this.Bind(m, f) = 
+        match m with 
+        | None -> 
+            printfn "Binding with None. Exiting."
+        | Some a -> 
+            printfn "Binding with Some(%A). Continuing" a
+        Option.bind f m
+
+    member this.Return(x) = 
+        Some x
+
+    member this.ReturnFrom(x) = 
+        x
+        
+    member this.Zero() = 
+        printfn "Zero"
+        //this.Return ()
+        None
+
+    member this.Delay(f) = 
+        printfn "Delay"
+        f
+
+    member this.Run(f) = 
+        f()
+
+    member this.While(guard, body) =
+        printfn "While: test"
+        if not (guard()) 
+        then 
+            printfn "While: zero"
+            this.Zero() 
+        else
+            printfn "While: body"
+            this.Bind( body(), fun () -> 
+                this.While(guard, body))
+
+type TraceBuilder() =
+    member this.Bind(m, f) = 
+        match m with 
+        | None -> 
+            printfn "Binding with None. Exiting."
+        | Some a -> 
+            printfn "Binding with Some(%A). Continuing" a
+        Option.bind f m
+
+    member this.Return(x) = 
+        printfn "Return an unwrapped %A as an option" x
+        Some x
+
+    member this.Zero() = 
+        printfn "Zero"
+        None
+
+    member this.Combine (m, f) = 
+        printfn "Combine. Starting second param %A" f
+        let y = f()
+        printfn "Combine. Finished second param %A. Result is %A" f y
+
+        match m,y with
+        | Some a, Some b ->
+            printfn "combining %A and %A" a b 
+            Some (a + b)
+        | Some a, None ->
+            printfn "combining %A with None" a 
+            Some a
+        | None, Some b ->
+            printfn "combining None with %A" b 
+            Some b
+        | None, None ->
+            printfn "combining None with None"
+            None
+
+    member this.Delay(funcToDelay) = 
+        let delayed = fun () ->
+            printfn "%A - Starting Delayed Fn." funcToDelay
+            let delayedResult = funcToDelay()
+            printfn "%A - Finished Delayed Fn. Result is %A" funcToDelay delayedResult
+            delayedResult
+        printfn "%A - Delaying using %A" funcToDelay delayed
+        delayed 
+
+    member this.Run(funcToRun) = 
+        printfn "%A - Run Start." funcToRun
+        let runResult = funcToRun()
+        printfn "%A - Run End. Result is %A" funcToRun runResult
+        runResult
+
+module EntryPoint =
+            
+        [<EntryPoint>]
+        let main argv =
+            let trace = TraceBuilder()
+            let m = trace { 
+                            printfn "Part 1: about to return 1"
+                            //return 1
+                            if true then
+                                let! x = Some 1
+                                printfn "inside if branch"
+                            else printfn "inside else branch"
+                            printfn "Part 2: after return has happened"
+                            printfn "AAAAAAAAAAAAAAA"
+                            return 2
+                            } 
+            0 
